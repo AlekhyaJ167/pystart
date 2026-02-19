5103ec3ada0dd929d5a64352c90fc6b782eee6fd
AlekhyaJ167 <ljampana@dons.usfca.edu>
2026-02-19 01:43:25 +0000

    finish my program

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from groq import Groq
 
-app = FastAPI()
+api_key = "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"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+client = Groq(api_key=api_key)
+
+while True:
+    answer = input("What do you want? ")
+    
+    if answer == 'quit':
+        break
+    
+    completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": answer
+            }
+        ],
+        temperature=1,
+        max_completion_tokens=8192,
+        top_p=1,
+        stream=False,
+        stop=None
+    )
+    
+    print(completion.choices[0].message.content)
+    
